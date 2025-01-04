6e4cd555fb658f9db68e3f5ed8af2971f0162a13
Duy <Lolmebunbun@gmail.com>
2025-01-03 21:41:17 -0600

    task creation & deletion

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-import { CreateToDoComponent } from '../create-to-do/create-to-do.component';
 @Component({
   selector: 'app-list',
-  imports: [CreateToDoComponent],
+  imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  tasks: Array<string> = ["task 1", "task 2", "task 3"]
+  tasks: Array<string> = [
+    "task 1", 
+    "task 2", 
+    "task 3"]
+
+  createTask(task: string){
+    if (task.length == 0){
+      alert("please put in a task")
+    } else if(task.length >= 100){
+      alert("task must be less than 100 characters")
+    } else {
+      this.tasks.push(task);
+    }
+
+
+  }
+
+  deleteTask(task: string){
+    let index = this.tasks.indexOf(task);
+    this.tasks.splice(index, 1);
+  }
 }
